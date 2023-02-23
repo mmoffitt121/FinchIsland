9f55b6aacc2c806feef70f8bbd5e72650d8076a1
Alejandro Camberos <45885170+alexcamb2@users.noreply.github.com>
2023-02-23 11:43:28 -0600

    Insane in the Bird Brain
    *Insane in the brain*

---
+++ Assets/scripts/Bird Brain/Main Brain Frame.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+/// <summary>
+/// This will be the place to set up the values for the bird movement
+/// </summary>
+public class MainBrainFrame : MonoBehaviour
+{
+
+}
+
+/// <summary>
+/// Making the bird move using values from the other area
+/// </summary>
+/// <param name="actionOut"></param>
+public override void Heuristic(in ActionBuffers actionsOut)
+{
+
+}
