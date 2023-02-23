1988438ec229a06cc643dfd45f049a7c27474fe3
Alejandro Camberos <45885170+alexcamb2@users.noreply.github.com>
2023-02-23 13:10:19 -0600

    Making it move pt2

--- Assets/scripts/Bird Brain/Main Brain Frame.cs
+++ Assets/scripts/Bird Brain/Main Brain Frame.cs
@@ -9,16 +9,70 @@ using UnityEngine;
 /// <summary>
 /// This will be the place to set up the values for the bird movement
 /// </summary>
-public class MainBrainFrame : MonoBehaviour
+public class MainBrainFrame : Agent
 {
+    
+    public float moveForce = 2f;
+    
+    public float pitchSpeed = 100f;
 
-}
+    public float yawSpeed = 100f;
 
-/// <summary>
-/// Making the bird move using values from the other area
-/// </summary>
-/// <param name="actionOut"></param>
-public override void Heuristic(in ActionBuffers actionsOut)
-{
+
+    /// <summary>
+    /// making the bird move
+    /// </summary>
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continousActionsOut = actionsOut.ContinuousActions;
+
+        Vector3 forward = Vector3.zero;
+        Vector3 left = Vector3.zero;
+        Vector3 up = Vector3.zero;
+        float pitch = 0f;
+        float yaw = 0f;
+
+        //Forward and back
+        if (Input.GetKey(KeyCode.W)) forward = transform.forward;
+        else  if (Input.GetKey(KeyCode.S)) forward = -transform.forward;
+
+        //left/right
+
+        if (Input.GetKey(KeyCode.A)) left = -transform.right;
+        else if (Input.GetKey(KeyCode.D)) left = transform.right;
+
+        //Up/Down
+        if (Input.GetKey(KeyCode.E)) up = transform.up;
+        else if (Input.GetKey(KeyCode.C)) up = -transform.up;
+
+        //Pitch up/down
+        if (Input.GetKey(KeyCode.UpArrow)) pitch = 1f;
+        else if (Input.GetKey(KeyCode.DownArrow)) pitch = -1f;
+
+        //Turn Left/Right
+        if (Input.GetKey(KeyCode.LeftArrow)) yaw = -1f;
+        else if (Input.GetKey(KeyCode.RightArrow)) yaw = 1f;
+
+        Vector3 combined = (forward + left + up).normalized;
+
+        continousActionsOut[0] = combined.x;
+        continousActionsOut[1] = combined.y;
+        continousActionsOut[2] = combined.z;
+        continousActionsOut[3] = pitch;
+        continousActionsOut[4] = yaw;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
