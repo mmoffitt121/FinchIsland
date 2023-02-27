4a6ffe4fa41d5dec22b862352f4f935e3447aa5e
loganmays <loganmays6@gmail.com>
2023-02-27 17:16:25 -0600

    Lesson/designer page
    did some ui changes to lesson page and got the remove button to work on design page

--- Assets/Scripts/LessonPlans/LessonPlanUI.cs
+++ Assets/Scripts/LessonPlans/LessonPlanUI.cs
@@ -10,8 +10,13 @@ public class LessonPlanUI : MonoBehaviour
     {
         GameObject lp = Instantiate(lessonPlanPrefab);
         lp.transform.SetParent(lessonPlanList.transform, false);
-    }
 
+    }
+    public void DeleteLessonPlan() 
+    {
+        Destroy(this.gameObject);
+
+    }
     public void EditLessonPlan(LessonPlan lp)
     {
 
