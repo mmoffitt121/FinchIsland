17abe82a6d100a70d7d580bf5c052dfbe7c2a045
Matthew Moffitt <mmoffitt121@gmail.com>
2023-03-28 13:57:05 -0500

    scripting

---
+++ Assets/scripts/Simulation Scripting/SimulationManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimulationManager : MonoBehaviour
+{
+    public GameObject setupEnvironmentStep;
+    public GameObject runSimulationStep;
+    public GameObject viewResultsStep;
+
+    public Button nextButton;
+    public TextMeshProUGUI stepLabel;
+
+    public SimulationStep step;
+
+    public void AdvanceStep(int stepTo = -1)
+    {
+        // If we don't specify a step
+        if (stepTo == -1)
+        {
+            // Moves to next step
+            step = (SimulationStep)(((int)step + 1) % 3);
+        }
+        // If we specify a step
+        else if (0 <= stepTo && stepTo < 3)
+        {
+            step = (SimulationStep)stepTo;
+        }
+
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        DisableUIElements();
+        switch (step)
+        {
+            case SimulationStep.SetupEnvironment:
+                DisplaySetupEnvironmentUI();
+                break;
+            case SimulationStep.RunSimulation:
+                DisplayRunSimulationUI();
+                break;
+            case SimulationStep.ViewResults:
+                DisplayViewResultsUI();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void DisplaySetupEnvironmentUI()
+    {
+        stepLabel.text = "Set Up Environment";
+        setupEnvironmentStep.SetActive(true);
+    }
+
+    public void DisplayRunSimulationUI()
+    {
+        stepLabel.text = "Simulation Running...";
+        runSimulationStep.SetActive(true);
+    }
+
+    public void DisplayViewResultsUI()
+    {
+        stepLabel.text = "Results";
+        viewResultsStep.SetActive(true);
+    }
+
+    public void DisableUIElements()
+    {
+        setupEnvironmentStep.SetActive(false);
+        runSimulationStep.SetActive(false);
+        viewResultsStep.SetActive(false);
+    }
+
+    public void Start()
+    {
+        UpdateUI();
+    }
+
+    public enum SimulationStep
+    {
+        SetupEnvironment = 0,
+        RunSimulation = 1,
+        ViewResults = 2
+    }
+}
