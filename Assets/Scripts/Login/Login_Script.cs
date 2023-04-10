65ea324f1a2a6012977000accb9a091801cc8922
hgallina44 <harrisonreidgallina@gmail.com>
2023-04-09 20:13:17 -0500

    Login Page
    Almost functioning Login Page.

--- Assets/Scripts/Login/Login_Script.cs
+++ Assets/Scripts/Login/Login_Script.cs
@@ -1,19 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using Encryption;
 using System;
+using TMPro;
 
 public class Login_Script : MonoBehaviour
 {
     // Start is called before the first frame update
-    public UnityEngine.UI.InputField usernameField;
-    public UnityEngine.UI.InputField passwordField;
+    public TMP_InputField usernameField;
+    public TMP_InputField passwordField;
+    public Button loginButton;
     public PasswordManager passwordManager;
-    public async void captureCredentials(UnityEngine.UI.InputField usernameField, UnityEngine.UI.InputField passwordField) 
-    {
-      bool isLoggedIn = await passwordManager.Login(usernameField.text, passwordField.text);
 
+
+    void Start()
+    {
+        //Subscribe to onClick event
+        loginButton.onClick.AddListener(captureCredentials);
+    }
+
+    public async void captureCredentials() 
+    {
+        string userName = usernameField.text;
+        string password = passwordField.text;
+        passwordManager = new PasswordManager();
+
+        Debug.Log("usernameField: " + userName);
+        Debug.Log("passwordField: " + password);
+        Debug.Log("loginButton: " + loginButton);
+        Debug.Log("passwordManager: " + passwordManager);
+
+        bool isLoggedIn = await passwordManager.Login(userName, password);
         Debug.Log(isLoggedIn);
     }
 }
