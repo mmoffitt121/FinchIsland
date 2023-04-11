13b6629f52860e46cd40d52eae7b64e620991f19
Matthew Moffitt <mmoffitt121@gmail.com>
2023-04-11 10:50:28 -0500

    Placeable objects

---
+++ Assets/Scripts/Simulation Scripting/SimulationResourceHolder.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationResourceHolder : MonoBehaviour
+{
+    public GameObject[] possibleResources;
+    public int selected;
+}
