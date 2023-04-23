bae2536a90ef65ddf3e6972c37358641bbcd7097
Matthew Moffitt <mmoffitt121@gmail.com>
2023-04-23 15:54:53 -0500

    UI sadness

---
+++ Assets/Scripts/Simulation Scripting/PlaceableItemUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaceableItemUI : MonoBehaviour
+{
+    // Contains the name of the item.
+    public TextMeshProUGUI itemNameField;
+    // Button that the user can select to place the object.
+    public Button placeButton;
+
+    // Two images that will be used to show the user whether this object is being placed or not.
+    public GameObject placing;
+    public int index;
+
+    public void SetName(string name)
+    {
+        itemNameField.text = name;
+    }
+
+    public void Select()
+    {
+        FindObjectOfType<SimulationResourceHolder>().SetSelected(index);
+    }
+
+    public void Deselect()
+    {
+        placing.SetActive(false);
+    }
+}
--- Assets/Scripts/Simulation Scripting/SimulationResourceHolder.cs
+++ Assets/Scripts/Simulation Scripting/SimulationResourceHolder.cs
@@ -1,10 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimulationResourceHolder : MonoBehaviour
 {
+    public GameObject itemLayoutGroup;
+    public GameObject itemUIPrefab;
     public GameObject[] possibleResources;
     public GameObject bird;
     public int selected;
+
+    public void SetSelected(int selected)
+    {
+        this.selected = selected;
+    }
+
+    public void LoadObjectOptions()
+    {
+        int i = 0;
+        foreach (GameObject item in possibleResources)
+        {
+            GameObject itemObject = Instantiate(itemUIPrefab);
+            itemObject.transform.SetParent(itemLayoutGroup.transform, false);
+            itemObject.GetComponent<PlaceableItemUI>().index = i;
+            itemObject.GetComponent<PlaceableItemUI>().SetName(item.name);
+            i++;
+        }
+    }
+
+    private void Start()
+    {
+        LoadObjectOptions();
+    }
 }
