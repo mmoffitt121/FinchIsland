3c90de1fb23f516ffe68b26746a12750ccb4fc98
hgallina44 <harrisonreidgallina@gmail.com>
2023-04-24 15:51:58 -0500

    Commenting Code
    I commented the code I wrote

--- Assets/Scripts/UI/SceneChanger.cs
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class SceneChanger : MonoBehaviour
 {
+    //Change to the scene specified by the parameter. The key for the ints is in the build settings.
     public void ChangeToScene(int sceneToChangeTo)
     {
         Application.LoadLevel(sceneToChangeTo);
